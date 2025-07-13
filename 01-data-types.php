e7d201d47a45b888754c3cdeba24ce66e205830a
anav5704 <anavkki@gmail.com>
2025-07-13 22:07:06 +1200

    feat: add data types

---
+++ 01-data-types.php
@@ -0,0 +1,20 @@
+<?php
+$name = 'Anav';
+$age = 21;
+$savings = 10.50;
+$employed = true;
+
+echo $name, $age, $savings, $employed;
+
+$sum = 5 + 5;
+$difference = 5 - 5;
+$product = 5 * 5;
+$quotient = 10 / 2;
+
+echo $sum, $difference, $product, $quotient;
+
+define("HOST", "localhost");
+define("PORT", "8080");
+
+echo HOST, PORT;
+?>
